e5c2dc17479d7a9f769c9e56aa1b4d997254642f
Merkucios <hydra1337channel@gmail.com>
2023-12-03 02:19:26 +1000

    Initial - created frontend application За основу клиента взят Next.js за его гибкость, реактивность и рендеринг (SSR, SSG, CSR)

---
+++ devrelcrm-webapp/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <h1>Bootstrap check</h1>
+      <button className='btn btn-primary'>Click Me</button>
+    </div>
+  )
+}
+
